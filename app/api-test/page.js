498e5372a62dad0a8a21d5b1497247b41847fa98
haidonovan <actionboyz2345@gmail.com>
2025-07-18 00:37:12 +0700

    Describe what you changed

--- app/api-test/page.js
+++ app/api-test/page.js
@@ -17,6 +17,12 @@ export default function ApiTestPage() {
     setResponse(text);
   }
 
+    async function handleHi() {
+    const res = await fetch("/api/hi");
+    const text = await res.text(); // or res.json() if JSON
+    setResponse(text);
+  }
+
   return (
     <div style={{ padding: 20 }}>
       <button onClick={handleClick} style={{ padding: "10px 20px", fontSize: 16 }}>
@@ -25,6 +31,10 @@ export default function ApiTestPage() {
 
       <button onClick={handlePick} style={{ padding: "10px 20px", fontSize: 16 }}>
         PICK
+      </button>
+
+      <button onClick={handleHi} style={{ padding: "10px 20px", fontSize: 16 }}>
+        Hi To the Server
       </button>
 
       {response && (
---
+++ app/api/hi/route.js
@@ -0,0 +1,3 @@
+export async function GET(request){
+    return new Response("Hi Bruhhh you are in your good day", {status: 200});
+};
